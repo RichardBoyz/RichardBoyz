59387557ddb481a7aab5a2075cdb1e3fee224894
richardboyz <r1382333@gmail.com>
2023-06-12 00:15:09 +0800

    Add required svg

--- assets/index.ts
+++ assets/index.ts
@@ -1,1 +1,6 @@
 export { default as FileText } from "./file-text.svg";
+export { default as Flutter } from "./flutter.svg";
+export { default as Nodejs } from "./nodejs.svg";
+export { default as Nestjs } from "./nestjs.svg";
+export { default as Vuejs } from "./vuejs.svg";
+export { default as Django } from "./django.svg";
