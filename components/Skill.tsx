51b01dc1ef64e492d07842175f98609b1b31324f
richardboyz <r1382333@gmail.com>
2023-06-18 02:27:01 +0800

    Adjust skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -3,18 +3,20 @@ import React from "react";
 
 type Props = {
   directionLeft?: boolean;
+  url?: string;
+  degree?: number;
 };
 
-function Skill({ directionLeft = false }: Props) {
+function Skill({ directionLeft = false, url = "", degree = 60 }: Props) {
   return (
     <div className=" group relative flex cursor-pointer">
       <motion.img
         initial={{ x: directionLeft ? 1 : 0, opacity: 0 }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-        src="https://images.ctfassets.net/ooa29xqb8tix/RrX9HCiZ8qPoIpJSlHphR/f9778b44e2b768d31fafb4ac70956682/vue-logo.png?w=400&q=50"
+        src={url}
         className="rounded-full border border-gray-500 
-        object-cover w-14 h-14 md:w-24 md:h-24 xl:w-32 xl:h-32
+         object-contain w-14 h-14 md:w-24 md:h-24 xl:w-32 xl:h-32
         filter group-hover:grayscale transition duration-300 ease-in-out
         "
       />
@@ -23,7 +25,9 @@ function Skill({ directionLeft = false }: Props) {
        ease-in-out group-hover:bg-white w-full h-full rounded-full z-0"
       >
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+          <p className=" text-lg md:text-3xl font-bold text-black opacity-100">
+            {degree}%
+          </p>
         </div>
       </div>
     </div>
