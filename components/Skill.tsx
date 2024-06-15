d40aecf8bdcb693dfff371bfcd4584aac92ba085
RichardBoyz <r1382333@gmail.com>
2024-06-15 17:17:33 +0800

    Disable skill degree

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -17,19 +17,19 @@ function Skill({ directionLeft = false, url = "", degree = 60 }: Props) {
         src={url}
         className="rounded-full border border-gray-500 
          object-contain w-14 h-14 md:w-24 md:h-24 xl:w-28 xl:h-28
-        filter group-hover:grayscale transition duration-300 ease-in-out
+        filter
         "
       />
-      <div
-        className="absolute opacity-0 group-hover:opacity-80 transition duration-300 
-       ease-in-out group-hover:bg-white w-full h-full rounded-full z-0"
-      >
-        <div className="flex items-center justify-center h-full">
-          <p className=" text-lg md:text-3xl font-bold text-black opacity-100">
-            {degree}%
-          </p>
-        </div>
-      </div>
+      {/* <div */}
+      {/*   className="absolute opacity-0 group-hover:opacity-80 transition duration-300  */}
+      {/*  ease-in-out group-hover:bg-white w-full h-full rounded-full z-0" */}
+      {/* > */}
+      {/*   <div className="flex items-center justify-center h-full"> */}
+      {/*     <p className=" text-lg md:text-3xl font-bold text-black opacity-100"> */}
+      {/*       {degree}% */}
+      {/*     </p> */}
+      {/*   </div> */}
+      {/* </div> */}
     </div>
   );
 }
